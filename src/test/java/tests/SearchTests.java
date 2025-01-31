85dfbf9dfb5e15071abbadeb02efc249f80957f4
Maksim Skomorokhov <nullnvrsk@gmail.com>
2025-01-31 12:40:08 +0300

    Refactoring baseTest, add steps

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -8,13 +8,18 @@ import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.*;
+import static io.qameta.allure.Allure.step;
 
 public class SearchTests extends TestBase {
 
     @Test
     void successfulSearchTest() throws MalformedURLException, InterruptedException {
-        $(accessibilityId("Search Wikipedia")).click();
-        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
-        $$(className("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () -> {
+            $$(className("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
     }
 }
